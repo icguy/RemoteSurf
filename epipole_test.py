f1b2c6644b1c8272c9eacca9d5d3c556584ab46a
icguy <ayhandani@hotmail.com>
2016-07-29 11:46:20 +0200

    added epiline test

--- epipole_test.py
+++ epipole_test.py
@@ -48,22 +48,22 @@ def calc_test_points(cam1, cam2, obj_pt, trf1, trf2):
 if __name__ == '__main__':
     test_epip()
 
-    import cv2, FeatureLoader as FL, MarkerDetect as MD
+    import cv2, FeatureLoader as FL, MarkerDetect as MD, Utils as util, MatchLoader as ML
     files = ["imgs/005.jpg", "imgs/006.jpg"]
     imgs = [cv2.imread(f) for f in files]
     fl = FL.FeatureLoader()
     kpts = [fl.loadFeatures(f, "surf") for f in files]
-    kpt_list = kpts[0]
+    ml = ML.MatchLoader()
+    matches = ml.loadMatches(files[0], files[1], None, None, "surf", ML.MATCHER_BF_CROSS, "mask")
 
-    img1 = cv2.pyrDown(imgs[0])
-    img2 = cv2.pyrDown(imgs[1])
+    img1 = imgs[0]
+    img2 = imgs[1]
 
     tmats = [MD.loadMat(f) for f in files]
-    
+    cam = util.camMtx
+    E, F = util.calcEssentialFundamentalMat(cam, cam, tmats[0], tmats[1])
 
-    for kpt in kpt_list:
-        realscale = 2
-
+    for m in matches:
         h, w, c = img1.shape
         out = np.zeros((h, w * 2, 3), np.uint8)
         out[:, :w, :] = img1
@@ -71,7 +71,32 @@ if __name__ == '__main__':
 
         color = (255, 255, 0)  # cyan
 
-        pt1 = kpt.pt
-        p1 = (int(pt1[0] / realscale), int(pt1[1] / realscale))
-        cv2.circle(out, p1, 10, color, 1)
+        kpt1 = kpts[0][0][m.queryIdx]
+        kpt2 = kpts[1][0][m.trainIdx]
+
+        pt1 = kpt1.pt
+        pt2 = kpt2.pt
+        p1 = (int(pt1[0]), int(pt1[1]))
+        p2 = (int(pt2[0] + w), int(pt2[1]))
+        pt1_h = np.ones((3, 1))
+        pt1_h[0] = pt1[0]
+        pt1_h[1] = pt1[1]
+        pt1_h = pt1_h.T
+        n = np.dot(pt1_h, F.T)
+        nx, ny, nz = n[0, 0], n[0, 1], n[0, 2]
+
+        u1 = 0
+        u2 = w
+        v1 = int((-nz-nx*u1)/ny)
+        v2 = int((-nz-nx*u2)/ny)
+
+        thk = 8
+        cv2.circle(out, p1, 40, color, thk)
+        cv2.circle(out, p2, 40, color, thk)
+        cv2.line(out, (u1 + w, v1), (u2 + w, v2), color, thk)
+
+        out = cv2.pyrDown(out)
+        out = cv2.pyrDown(out)
+
         cv2.imshow("match", out)
+        cv2.waitKey()
