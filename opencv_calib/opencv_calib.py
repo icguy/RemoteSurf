dfeb821c930fc93789e13b6308ec4ed222558e82
icguy <ayhandani@hotmail.com>
2017-02-07 11:48:28 +0100

    removed legacy code

--- opencv_calib/opencv_calib.py
+++ opencv_calib/opencv_calib.py
@@ -2,8 +2,13 @@ import numpy as np
 import cv2
 import glob
 
+
+np.set_printoptions(precision=5, suppress=True)
+
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+
+outfile = file("out.txt", "w")
 
 grid_size = (29, 19)
 resolution = (1600, 1200)
@@ -21,6 +26,7 @@ downscale = 2
 objp = np.zeros((grid_size[0] * grid_size[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:grid_size[0], 0:grid_size[1]].T.reshape(-1,2)
 objp *= real_size
+print objp
 
 
 # Arrays to store object points and image points from all the images.
@@ -28,63 +34,27 @@ objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
 images = glob.glob(imgs_path)
-
-corner_dict = {
-    "imgset2\\Picture 11.jpg" : np.array([[326, 267], [1260, 272], [348, 908], [1377, 845]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 12.jpg" : np.array([[396, 359], [1144, 374], [381, 847], [1157, 853]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 13.jpg" : np.array([[458, 316], [1328, 306], [425, 849], [1409, 832]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 14.jpg" : np.array([[311, 636], [1034, 196], [646, 1084], [1438, 515]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 15.jpg" : np.array([[179, 633], [1036, 245], [506, 1105], [1498, 527]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 16.jpg" : np.array([[214, 266], [1394, 265], [169, 1038], [1442, 1040]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 17.jpg" : np.array([[320, 382], [1267, 386], [211, 896], [1385, 896]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 18.jpg" : np.array([[253, 605], [1220, 517], [135, 1095], [1409, 974]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 19.jpg" : np.array([[523, 417], [1323, 142], [432, 991], [1294, 957]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 20.jpg" : np.array([[590, 231], [1462, 297], [227, 715], [1045, 1039]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 21.jpg" : np.array([[520, 251], [1268, 515], [194, 665], [938, 1068]], dtype='float64').reshape((4, 2)),
-    "imgset2\\Picture 22.jpg" : np.array([[537, 340], [1421, 438], [377, 851], [1379, 1012]], dtype='float64').reshape((4, 2)),
-}
+print images
 
 fnames = []
 # images = ["imgset2\\Picture 15.jpg"]
+num = -1
 for fname in images:
+    num += 1
     img = cv2.imread(fname)
 
-    if fname not in corner_dict:
-        scale = 1
-        gray = img
-        for i in range(downscale - 1):
-            gray = cv2.pyrDown(gray)
-            scale *= 2
-        gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
 
-        ret, corners = cv2.findChessboardCorners(gray, grid_size, flags=cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE)
-        if corners is not None:
-            corners *= scale
-
-        print ("+ " if ret else "- ") + fname
-    else:
-        corners = corner_dict[fname]
-        mtx = np.array([1.8435610863515064e+003, 0., 799.5, 0., 1.8435610863515064e+003, 599.5, 0., 0., 1.]).reshape((3, 3))
-        obj_pts = np.array([[.0, .0, 0], [28, .0, 0], [0, 18, 0], [28, 18, 0]]) * .95
-        retval, rvec, tvec = cv2.solvePnP(obj_pts, corners, mtx, None)
-        corners, _ = cv2.projectPoints(objp, rvec, tvec, mtx, None)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        img2 = img
-        cv2.drawChessboardCorners(img2, grid_size, corners, True)
-        corners = corners
-        img2 = cv2.pyrDown(img2)
-        cv2.imshow("", img2)
-        cv2.waitKey(0)
-
-        ret = True
-        print ("+ " if ret else "- ") + fname
+    ret, corners = cv2.findChessboardCorners(gray, grid_size, flags=cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE)
+    outfile.write("%d \r\n %s \r\r %s \r\n" % (num , "---- corners" , str(corners)))
+    print ("+ " if ret else "- ") + fname
 
     if ret:
         fnames.append(fname)
         objpoints.append(objp)
 
         cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        outfile.write("%d \r\n %s \r\r %s \r\n" % (num , "---- corners subpix" , str(corners)))
         imgpoints.append(corners)
 
         img2 = cv2.pyrDown(img)
@@ -93,7 +63,9 @@ for fname in images:
         cv2.waitKey(1)
         cv2.destroyWindow("img")
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, resolution, flags=cv2.CALIB_FIX_ASPECT_RATIO | cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_ZERO_TANGENT_DIST)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, resolution, flags=cv2.CALIB_FIX_ASPECT_RATIO | cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_ZERO_TANGENT_DIST | cv2.CALIB_FIX_PRINCIPAL_POINT)
+
+outfile.write("mat, distcoeff \r\n %s \r\n .. \r\n %s" % (str(mtx), str(dist)))
 print ret
 print mtx
 print dist
@@ -111,3 +83,5 @@ for i in xrange(len(objpoints)):
 
 print "total error: ", mean_error/len(objpoints)
 print len(objpoints)
+
+outfile.close()
