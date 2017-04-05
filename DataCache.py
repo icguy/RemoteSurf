39166f05e3bf20a333e95d4cf42accc784867399
icguy <ayhandani@hotmail.com>
2017-04-05 07:07:04 +0200

    m

--- DataCache.py
+++ DataCache.py
@@ -7,6 +7,7 @@ POINTS4D_TRIANGULATE = "cache/points4d_triangulate.p" # epilines + triangulate
 POINTS4D_UNIFIED = "cache/points4d_unified.p"   # POINTS4D_TRIANGULATE + POINTS4D
 POINTS4D_HOMOGR_TRIANG_SIMPLE = "cache/points4d_homogr_triang_simple.p"   # epilines homogr + triang simple
 POINTS4D_HOMOGR_TRIANG = "cache/points4d_homogr_triang.p"   # epilines homogr + triang
+POINTS4D_HOMOGR_TRIANG_RANSAC = "cache/points4d_homogr_triang_ransac.p"   # epilines homogr + triang ransac
 
 # DO NOT USE
 POINTS4D_UNIFIED_ALL = "cache/points4d_unified_all.p"   # POINTS4D_TRIANGULATE + POINTS4D + POINTS4D_MULTIPLE_MATCH
